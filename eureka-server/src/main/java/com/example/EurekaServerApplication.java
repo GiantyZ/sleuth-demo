f68ea8d6eb24c782750ed1ae887e4497c855afa0
GiantyZ <757528555@qq.com>
2021-05-12 22:01:17 +0800

    sleuth demo update

--- eureka-server/src/main/java/com/example/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/example/EurekaServerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-// 开启 EurekaServer 注解
 @EnableEurekaServer
 public class EurekaServerApplication {
 
